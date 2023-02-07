3879436aec0e898a9f87158e8a5a635abd863e74
josipnigojevic <jnigoj00@fesb.hr>
2023-02-07 14:38:59 +0100

    final changes added to the code

--- Client/Robot_software.c/Robot_software.c.ino
+++ Client/Robot_software.c/Robot_software.c.ino
@@ -1,69 +1,61 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-
 #include <esp_now.h>
 #include <WiFi.h>
 #include <ESP32Servo.h>
-
 Servo myservo;
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
-// Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
-
+Servo myservo2;
 int servoPin = 26;
+int servoPin2 =27;
 
-//Structure example to receive data
-//Must match the sender structure
 typedef struct test_struct {
-  int x;
+  bool x,y;
 } test_struct;
 
-//Create a struct_message called myData
 test_struct myData;
 
-//callback function that will be executed when data is received
+
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Bytes received: ");
   Serial.println(len);
   Serial.print("x: ");
   Serial.println(myData.x);
+  Serial.print("y: ");
+  Serial.println(myData.y);
 }
  
 void setup() {
-  //Initialize Serial Monitor
   Serial.begin(115200);
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-  myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
+  myservo.setPeriodHertz(50);    
+  myservo.attach(servoPin, 1000, 2000); 
+  myservo2.attach(servoPin2, 1000, 2000); 
   //Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  //Init ESP-NOW
+
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
 }
  
 void loop() {
-if(myData.x > 50){
-  myservo.write(120); 
-  }else{
-    myservo.write(30); 
+if(myData.x == 1 && myData.y == 0){
+  myservo.write(120);
+  myservo2.write(120);
+  }else if(myData.x == 0 && myData.y == 1){
+  myservo2.write(30);
+  myservo.write(30);
+    }else if(myData.x == 1 && myData.y == 1){
+  myservo2.write(30);
+  myservo.write(120);
+    }else{
+  myservo2.write(120);
+  myservo.write(30);
     }
 }
--- Server/esp_test/esp_test.ino
+++ Server/esp_test/esp_test.ino
@@ -1,23 +1,23 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-// REPLACE WITH RECEIVER MAC Address
+
 // 94:B9:7E:EA:81:04
 uint8_t broadcastAddress[] = {0x94, 0xB9, 0x7E, 0xEA, 0x81, 0x04};
-const int SOFT_POT_PIN = A0;
-// Structure example to send data
-// Must match the receiver structure
+const int SOFT_POT_PIN = 5;
+const int SOFT_POT_PIN2 = 4;
+
 typedef struct struct_message {
-int x;
+bool x,y;
 } struct_message;
 
-// Create a struct_message called myData
+
 struct_message myData;
 
 unsigned long lastTime = 0;  
-unsigned long timerDelay = 20;  // send readings timer
+unsigned long timerDelay = 20;  
 
-// Callback when data is sent
+
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
@@ -29,33 +29,32 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 }
  
 void setup() {
-  // Init Serial Monitor
+
   Serial.begin(115200);
   pinMode(SOFT_POT_PIN, INPUT);
-  // Set device as a Wi-Fi Station
+  pinMode(SOFT_POT_PIN2, INPUT);
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
+
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
+
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
   
-  // Register peer
+
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
  
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
      
-    myData.x = analogRead(SOFT_POT_PIN);;
-
-    // Send message via ESP-NOW
+    myData.x = digitalRead(SOFT_POT_PIN);
+    myData.y = digitalRead(SOFT_POT_PIN2);
+    
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 
     lastTime = millis();
